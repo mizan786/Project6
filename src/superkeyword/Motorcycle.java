f91de12bf518375b97f7912466006a18fac52d6d
mizan786 <mrbaruch2014@gmail.com>
2017-04-08 16:48:45 -0400

    First try

---
+++ src/superkeyword/Motorcycle.java
@@ -0,0 +1,16 @@
+package superkeyword;
+
+public class Motorcycle {
+	String model;
+	int year;
+	
+	
+	public Motorcycle(){
+		System.out.println("This is Phone Object");
+	}
+
+	public Motorcycle(String model){
+		this.model = model;
+	}
+}
+
